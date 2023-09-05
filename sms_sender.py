0ed5c9db482c8f17240e8ec3ff448a82be6fa3d3
AshleyMush <tanaka.mush@gmail.com>
2023-09-05 01:55:56 +0100

    Initial Commit: "Initial commit for Snake game." "Starting the Snake game project."
    Added Basic Functionality:
    "Implemented basic movement for the snake."
    "Added food spawning and snake growth."
    
    Bug Fixes:
    
    "Fixed bug where snake could move in opposite direction."
    "Resolved issue with food spawning inside snake."

--- sms_sender.py
+++ sms_sender.py
@@ -8,17 +8,17 @@ print(otp)
 
 #--------------Twilio setup_____
 
-account_sid = 'YOUR SID'
-auth_token = 'YOUR TOKEN'
+account_sid = 'AC6f116151d21a4d82a76413bc5f07b6ca'
+auth_token = 'b498a4bb728a1093d016af30fa804f80'
 
-api_key = "YOUR KEY"
+api_key = "0d75e43670b2ffca4032c37ae7d31c39"
 #------------------------------------------
 client = Client(account_sid, auth_token)
 
 message = client.messages.create(
     body=f'Your verification code is: {otp}',
-    from_='YOUR NUM',
-    to='RECEIVER NUM'
+    from_='+447883304338',
+    to='+447478881797'
 )
 
 print(message.status)
